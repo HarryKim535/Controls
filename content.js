628ee0d3d657095a31df9aa56be90a2c57c8a4ce
Harry <harryj.kim1122@gmail.com>
2020-12-31 20:02:38 +0900

    space2tab

--- content.js
+++ content.js
@@ -4,79 +4,79 @@ let gain
 let source
 
 function setup () {
-    audioContext = new AudioContext()
-    compressor = audioContext.createDynamicsCompressor()
-    gain = audioContext.createGain()
-    source = audioContext.createMediaElementSource(getAudio())
-    source.connect(compressor)
-    compressor.connect(gain)
-    gain.connect(audioContext.destination)
+	audioContext = new AudioContext()
+	compressor = audioContext.createDynamicsCompressor()
+	gain = audioContext.createGain()
+	source = audioContext.createMediaElementSource(getAudio())
+	source.connect(compressor)
+	compressor.connect(gain)
+	gain.connect(audioContext.destination)
 }
 
 function getAudio () {
-    let audio = document.querySelector('video')
-    if (!audio) audio = document.querySelector('audio')
-    return audio
+	let audio = document.querySelector('video')
+	if (!audio) audio = document.querySelector('audio')
+	return audio
 }
 
 function setCompressor (values) {
-    for (let item in values) {
-        console.log(item, values[item])
-        if (item == 'gain') gain.gain.exponentialRampToValueAtTime(2**values[item], audioContext.currentTime + 0.2)
-        else compressor[item].linearRampToValueAtTime(values[item], audioContext.currentTime + 0.2)
-    }
+	for (let item in values) {
+		console.log(item, values[item])
+		if (item == 'gain') gain.gain.exponentialRampToValueAtTime(2**values[item], audioContext.currentTime + 0.2)
+		else compressor[item].linearRampToValueAtTime(values[item], audioContext.currentTime + 0.2)
+	}
 }
 
 function getUrl () {
-    return {
-        href : location.href,
-        host : location.hostname,
-        path : location.pathname,
-        search : location.search
-    }
+	return {
+		href : location.href,
+		host : location.hostname,
+		path : location.pathname,
+		search : location.search
+	}
 }
 
 
 function listener (message) {
-    if (message.to == 'content') {
-        if (message.from == 'popup') {
-            if (message.type == 'notification') {
-                if (message.content == 'popup loaded') {
-                    if (getAudio() === null) {
-                        send('popup', 'url', null)
-                    }
-                    else {
-                        if (!audioContext) setup()
-                        send('popup', 'url', getUrl())
-                    }
-                }
-            }
-            else if (message.type == 'apply') {
-                delete message.content.range
-                setCompressor(message.content)
-            }
-        }
-        else if (message.from == 'background') {
-            if (message.type == 'values') {
-                delete message.content.range
-                setCompressor(message.content)
-            }
-        }
-    }
+	if (message.to == 'content') {
+		if (message.from == 'popup') {
+			if (message.type == 'notification') {
+				if (message.content == 'popup loaded') {
+					if (getAudio() === null) {
+						send('popup', 'url', null)
+					}
+					else {
+						if (!audioContext) setup()
+						send('popup', 'url', getUrl())
+					}
+				}
+			}
+			else if (message.type == 'apply') {
+				delete message.content.range
+				setCompressor(message.content)
+			}
+		}
+		else if (message.from == 'background') {
+			if (message.type == 'values') {
+				delete message.content.range
+				setCompressor(message.content)
+			}
+		}
+	}
 }
 
 function send (to, type, content) {
-    chrome.runtime.sendMessage({to: to, from: 'content', type: type, content: content})
+	chrome.runtime.sendMessage({to: to, from: 'content', type: type, content: content})
 }
 
 window.onload = function () {
-    chrome.runtime.onMessage.addListener(listener)
-    send('popup', 'notification', 'window loaded')
-    if (!getAudio()) {
-        return
-    }
-    else {
-        setup()
-        send('background', 'values', getUrl())
-    }
+	chrome.runtime.onMessage.addListener(listener)
+	send('popup', 'notification', 'window loaded')
+	if (!getAudio()) {
+		return
+	}
+	else {
+		setup()
+		send('background', 'values', getUrl())
+	}
 }
